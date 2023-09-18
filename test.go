664ab7236310f81df0c533071f2dca96fe040b38
beastkasper <62433555+beastkasper@users.noreply.github.com>
2023-09-18 19:13:09 +0500

    first commit

---
+++ test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+// 	"time"
+)
+
+func main() {
+	// begin 31
+	// var tf float32
+	// fmt.Print("Введите температуру по фаренгейту для расчета в тельсии: ")
+	// fmt.Scan(&tf)
+	// fmt.Println("обработка...")
+	// time.Sleep(1 * time.Second)
+	// var tc = (tf - 32) * 5 / 9
+	// // fmt.Println("Ответ:", tc)
+	// des := fmt.Sprintf("\x1b[%dm%s\x1b[0m", 000, "Ответ:")
+	// res := fmt.Sprintf("\x1b[%dm%f\x1b[0m", 34, tc)
+  //   fmt.Println(des,res)
+
+	a := 8
+	b := 8
+	var c bool
+
+	if a == b {
+		fmt.Println(!c)
+	} else{
+		fmt.Println(c)
+	}
+
+}
